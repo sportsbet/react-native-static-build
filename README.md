dfbc9711b52e06541d5a7075985835b66ea80f51
Tyrone Trevorrow <Tyrone.Trevorrow@sportsbet.com.au>
2017-07-07 10:55:39 +1000

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,136 @@
+# React Native Static Build
+
+This repo provides the current (0.45) version of React Native statically
+compiled using the provided `build.sh` script. There are a number of
+advantages to doing this:
+
+* Don't have to compile & analyze(!) React Native every time you clean.
+* Faster CI/CD
+* Parallelize build in your scheme works again
+* No more weird issues with `#import <React/RCT*.h>`
+
+# Building a New Version
+
+Currently only works with **Xcode 8.3.x**. Make sure Xcode 8.3.x is your
+default toolchain by using `xcode-select`.
+
+First ensure your environment is clean.
+
+```sh
+./build.sh -clean
+```
+
+You can now check out the version of React Native you want using NPM/Yarn:
+
+```sh
+mkdir -p node_modules && npm install react-native@0.45
+```
+
+Build against this React Native module:
+
+```sh
+./build.sh -build node_modules/react-native
+```
+
+If there are any errors they'll appear in `build/xcodebuild.*.log`.
+
+After building, copy the `libReact.debug.a` and `libReact.release.a`, and the
+`include` folder into your own project's folder structure. These files are
+massive, usually over 100MB, so consider using Git LFS.
+
+# Integrating with Xcode
+
+There's a few ways you can do this, the way I know works is pretty complicated.
+
+`libReact.debug.a` should be used when you're doing debug development builds.
+It includes things like the React debugger, the shake gesture menu, and the
+CMD+R reload shortcut. Under no circumstances should you link against this for
+a production build, or you will almost certainly get rejected for private API
+usage.
+
+`libReact.release.a` should be used for production builds. It has profiling
+disabled, and doesn't include any private APIs or debugging features.
+
+Xcode doesn't support linking with a different library binary depending on
+build configuration out of the box, you can try to manually specify the library
+using just build settings, or:
+
+### 1. Create a new build phase
+
+This phase should execute *before* Compile Sources.
+
+Shell: `/bin/bash`
+
+```mkdir -p "$BUILT_PRODUCTS_DIR"
+rm -rf "$BUILT_PRODUCTS_DIR/libReact.a"
+if [ "$CONFIGURATION" = "Debug" ]; then
+    cp "$PROJECT_DIR/[path-to]/libReact.debug.a" "$BUILT_PRODUCTS_DIR/libReact.a"
+else
+    cp "$PROJECT_DIR/[path-to]/libReact.release.a" "$BUILT_PRODUCTS_DIR/libReact.a"
+fi
+```
+
+### 2. Build your app
+
+We need your app's derived data for the next step, so run the build and wait
+for it to finish.
+
+### 3. Add libReact.a from derived data
+
+Open your app's `$BUILT_PRODUCTS_DIR` in Finder. It's usually something like:
+
+	$HOME/Library/Developer/Xcode/DerivedData/[Target]-[robotbarf]/Build/Products/[BuildConfiguration]-[sdk]/
+
+Then, drag the `libReact.a` file from this folder into your Xcode Project
+Navigator, in whichever group you keep your libraries.
+
+**Do not check *copy files if needed*!**
+
+### 4. Edit your .pbxproj
+
+Xcode will include this library with absolute paths, so it won't work if you
+build configurations, or users. There is no way to change the location for
+*libraries* to relative paths, so you'll need to edit the xcodeproj manually.
+
+Open `[YourProject].xcodeproj/project.pbxproj` in your favourite text editor.
+
+Find the `Begin PBXFileReference section`.
+
+In this section, find `/* libReact.a */`. If you don't see `path =` on this
+same line, keep looking.
+
+Change the `path =` to just `path = libReact.a;`.
+
+Change `sourceTree =` to `sourceTree = BUILT_PRODUCTS_DIR;`
+
+### 5. Link with libReact.a
+
+In the Project Editor, in your target's General tab, click the `+` and add
+`libReact.a`.
+
+### 6. Add `include` to your Header Search Paths
+
+In the Build Settings for your target, find the **Header Search Paths** option,
+and add the path to your `include` dir. Should be something like:
+
+	$(PROJECT_DIR)/Libraries/react-native/include
+
+
+# Adding new React Native Libraries
+
+This build currently bundles the following React Native libraries together:
+
+* ActionSheetIOS
+* Geolocation
+* Image
+* LinkingIOS
+* NativeAnimation
+* Network
+* Settings
+* Text
+* Vibration
+* WebSocket
+
+If you need to add more, make sure it's available in the
+`node_modules/react-native/Libraries` folder, then just add it to the list in
+`build.sh` starting on line 24.
---
+++ build.sh
@@ -0,0 +1,203 @@
+#!/bin/bash
+
+# Immediately quit on error
+set -e
+
+SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P ) 
+OUT_BUILD_DIR="$SCRIPTPATH/build"
+REACT_ROOT="$2"
+SCRIPTNAME="$0"
+
+# Parameters:
+# 1. Name - The name of this script
+print_usage () {
+	local _underline=`tput smul`
+	local _nounderline=`tput rmul`
+	echo "USAGE"
+	echo "$0 -build ${_underline}reactnodemodulepath${_nounderline}"
+	echo ""
+	echo "$0 -clean"
+	echo ""
+	exit 1
+}
+
+REACT_LIBS=(
+	ActionSheetIOS
+	Geolocation
+	Image
+	LinkingIOS
+	NativeAnimation
+	Network
+	Settings
+	Text
+	Vibration
+	WebSocket
+)
+
+# Parameters:
+# 1. debug/release
+# 2. simulator/device
+# 3. Xcodeproj
+build () {
+	case "$1" in
+		debug)
+			local configuration="Debug"
+			;;
+		
+		release)
+			local configuration="Release"
+			;;
+
+		*)
+			print_usage "$SCRIPTNAME"
+	esac
+
+	case "$2" in
+		simulator)
+			local archs="x86_64"
+			local sdk="iphonesimulator10.3"
+			;;
+
+		device)
+			local archs="armv7 arm64"
+			local sdk="iphoneos10.3"
+			;;
+
+		*)
+			print_usage "$SCRIPTNAME"
+	esac
+	local xcodeproj="$3"
+	local target=$(basename "$xcodeproj" .xcodeproj)
+
+	local xcargs=(-project "$xcodeproj"
+		-target "$target"
+		-sdk "$sdk"
+		-configuration "$configuration"
+		build
+		RCT_NO_LAUNCH_PACKAGER=yes
+		RUN_CLANG_STATIC_ANALYZER=no
+		ARCHS="$archs"
+		ONLY_ACTIVE_ARCH=no
+		MODULE_CACHE_DIR="$OUT_BUILD_DIR/DerivedData/ModuleCache"
+		OBJROOT="$OUT_BUILD_DIR/Intermediates"
+		SHARED_PRECOMPS_DIR="$OUT_BUILD_DIR/Intermediates/PrecompiledHeaders"
+		SYMROOT="$OUT_BUILD_DIR/Products"
+	)
+	echo "xcodebuild ${xcargs[@]}"
+	xcodebuild "${xcargs[@]}" > "$OUT_BUILD_DIR/xcodebuild.$configuration.$2.log"
+}
+
+# Parameters
+# 1. Xcodeproj
+build_all_variants () {
+	local xcodeproj="$1"
+	build "debug" "simulator" "$xcodeproj"
+	build "release" "simulator" "$xcodeproj"
+	build "debug" "device" "$xcodeproj"
+	build "release" "device" "$xcodeproj"
+}
+
+# Parameters:
+# 1. debug/release
+# 2. simulator/device
+join_libraries () {
+	case "$1" in
+		debug)
+			local configuration="Debug"
+			;;
+		
+		release)
+			local configuration="Release"
+			;;
+
+		*)
+			print_usage "$SCRIPTNAME"
+	esac
+
+	local dest="$2"
+	case "$dest" in
+		simulator)
+			local build_dir="$OUT_BUILD_DIR/Products/$configuration-iphonesimulator"
+			;;
+
+		device)
+			local build_dir="$OUT_BUILD_DIR/Products/$configuration-iphoneos"
+			;;
+
+		*)
+			print_usage "$SCRIPTNAME"
+	esac
+
+	# Link all of React Native's .a files into a single .a file:
+	pushd "$build_dir" > /dev/null
+	echo "$PWD"
+	echo "libtool -static -o \"libReact.$configuration.a\" *.a"
+	find . -name '*.a' -depth 1 -print0 | xargs -0 libtool -static -o "libReact.$1.a" >> "$OUT_BUILD_DIR/xcodebuild.$configuration.$dest.log" 2>&1
+	popd > /dev/null
+}
+
+# Parameters
+# 1. debug/release
+pack_universal_binary () {
+	local configuration="$1"
+	case "$1" in
+		debug)
+			local device_build_dir="$OUT_BUILD_DIR/Products/Debug-iphonesimulator"
+			local sim_build_dir="$OUT_BUILD_DIR/Products/Debug-iphoneos"
+			;;
+		
+		release)
+			local device_build_dir="$OUT_BUILD_DIR/Products/Release-iphonesimulator"
+			local sim_build_dir="$OUT_BUILD_DIR/Products/Release-iphoneos"
+			;;
+
+		*)
+			print_usage "$SCRIPTNAME"
+	esac
+	
+	join_libraries "$configuration" "simulator"
+	join_libraries "$configuration" "device"	
+
+	# Join the device / simular .a files into a fat .a file:
+	local xcrun_args=(
+		-sdk iphoneos 
+		lipo -create 
+		-output "$OUT_BUILD_DIR/libReact.$configuration.a"
+		"$device_build_dir/libReact.$configuration.a"
+		"$sim_build_dir/libReact.$configuration.a"
+	)
+	echo "xcrun ${xcrun_args[@]}"
+	xcrun "${xcrun_args[@]}"
+}
+
+case "$1" in
+	-build)
+		mkdir -p "$OUT_BUILD_DIR"
+
+		if [ -z "$REACT_ROOT" ] || [ ! -e "$REACT_ROOT" ] ; then
+			print_usage $0
+		fi
+
+		build_all_variants "$REACT_ROOT/React/React.xcodeproj"
+
+		for react_lib in "${REACT_LIBS[@]}"; do
+			libdir="$REACT_ROOT/Libraries/$react_lib"
+			xcodeproj=$(find "$libdir" -name '*.xcodeproj' | head -n 1)
+			build_all_variants "$xcodeproj"
+		done
+
+		pack_universal_binary "debug"
+		pack_universal_binary "release"
+		rm -rf libReact.release.a libReact.debug.a include
+		cp "$OUT_BUILD_DIR/libReact.debug.a" "$OUT_BUILD_DIR/libReact.release.a" .
+		cp -R "$OUT_BUILD_DIR/Products/Release-iphoneos/include" .
+		;;
+
+	-clean)
+		rm -rf "$OUT_BUILD_DIR"
+		;;
+
+	*)
+		print_usage "$0"
+esac
+
